a28b8fdda001e50f7381733db28ca88be4b487e0
manu_ <manu_@LAPTOP-7HTT7HMN>
2025-06-22 08:29:22 -0600

    dogmenu-started

--- Pets/Dogs/urls.py
+++ Pets/Dogs/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('dog-menu/',views.DogsMenu,name='dog-menu'),
+    path('dog-find',views.DogFind,name='dog-find'),
+
+
 ]
 
 
--- Pets/Dogs/views.py
+++ Pets/Dogs/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+
+def DogsMenu(request):
+    try:
+
+
+        return render(request,'dog-menu.html')
+    except Exception as e:
+        print(e)
+
+
+
+def DogFind(request):
+    try:
+        
+
+        
+
+        return render(request,'dog-find.html')
+    except Exception as e:
+        print(e)
